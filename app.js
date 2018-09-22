a34940b8b3e3b42a28aaf02ca934af7744c987a7
Charpell <charpellumeh@gmail.com>
2018-09-22 20:19:07 +0100

    Delete ideas

--- app.js
+++ app.js
@@ -123,6 +123,13 @@ app.put('/ideas/:id', (req, res) => {
   });
 });
 
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+  Idea.remove({_id: req.params.id})
+    .then(() => {
+      res.redirect('/ideas');
+    });
+});
 
 const port = process.env.PORT || 5000;
 
