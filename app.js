75a6b170cf64170e83658d342778f53fd40358dd
Charpell <charpellumeh@gmail.com>
2018-09-22 19:46:22 +0100

    Create Idea Model

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://vidjot:vidjot1@ds111963.mlab.com:11963/vidjot', {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('Err', err));
+
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
