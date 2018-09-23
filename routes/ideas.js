f936bf2eef32fafe5a91f35333d243d4a46800c6
Charpell <charpellumeh@gmail.com>
2018-09-23 06:56:35 +0100

    Access Contol for Ideas

--- routes/ideas.js
+++ routes/ideas.js
@@ -11,7 +11,7 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-  Idea.find({})
+  Idea.find({ user: req.user.id })
     .sort({date:'desc'})
     .then(ideas => {
       res.render('ideas/index', {
@@ -41,7 +41,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
     res.render('ideas/add', {
       errors: errors,
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     });
   } else {
     const newUser = {
@@ -63,9 +64,15 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     _id: req.params.id
   })
   .then(idea => {
-    res.render('ideas/edit', {
-      idea:idea
-    });
+    if(idea.user != req.user.id){
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', {
+        idea:idea
+      });
+    }
+    
   });
 });
 
